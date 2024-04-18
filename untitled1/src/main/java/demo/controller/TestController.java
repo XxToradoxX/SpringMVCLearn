d28fe2ee3658856d22d2e5507e24a9c6267c7d81
tT <1784264314@qq.com>
2024-04-18 15:50:58 +0800

    04181526

--- untitled1/src/main/java/demo/controller/TestController.java
+++ untitled1/src/main/java/demo/controller/TestController.java
@@ -1,4 +1,12 @@
 package demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class TestController {
+    @RequestMapping("/ttst")
+    public String ttst1(){
+        return "wtf";
+    }
 }
