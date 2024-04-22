78a49f277e77a2949cd9dc28ce0199172d5cdbc6
Administrator <1784264314@qq.com>
2024-04-22 11:26:04 +0800

    0900

--- untitled1/src/main/java/demo/controller/TestController.java
+++ untitled1/src/main/java/demo/controller/TestController.java
@@ -5,23 +5,25 @@ import netscape.javascript.JSObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class TestController {
     @RequestMapping("/hello")
     public String ttst1(){
         return "hello";
     }
     @RequestMapping("/ttst2")
-    @ResponseBody
+//    @ResponseBody
     public Demo ttst2(){
 
-        String str = "{'age':'18','name':'ll'}";
+//        String str = "{'age':'18','name':'ll'}";
         return new Demo("lhh",18);
+//        return str;
     }
 }
