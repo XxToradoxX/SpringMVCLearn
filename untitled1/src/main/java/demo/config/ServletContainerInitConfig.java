544a65c03b656e7417b38a34233886934052b644
Administrator <1784264314@qq.com>
2024-04-24 09:47:42 +0800

    0915

---
+++ untitled1/src/main/java/demo/config/ServletContainerInitConfig.java
@@ -0,0 +1,24 @@
+package demo.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ServletContainerInitConfig extends AbstractDispatcherServletInitializer {
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringmvcConfig.class);
+        return context;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+}
