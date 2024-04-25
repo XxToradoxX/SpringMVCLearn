8a7a5ee37ed49bd71c0d3beb29769a50a566d64a
Administrator <1784264314@qq.com>
2024-04-25 14:39:52 +0800

    1342

--- untitled1/src/main/java/demo/config/ServletContainerInitConfig.java
+++ untitled1/src/main/java/demo/config/ServletContainerInitConfig.java
@@ -1,32 +1,20 @@
 package demo.config;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
-//其次它里面有方法可以返回WebApplicationContext的对象。
-//所以说，实现这个类，反过来会查找实现WebApplicationContext的类并将配置的任务交给他们来完成。
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-//其中方法createRootApplicationContext和createServletApplicationContext都是用来创建程序的引用上下文
-//只不过一个是Root的一个是servlet的。
-// 对于获取到的配置类型信息数组做出判断，如果不为null,则调用AnnotationConfigWebApplicationContext类的register方法进行注册。
-//其中getServletConfigClasses()
-//方法返回带有@Configuration注解的类将会用来定义DispatcherServlet应用上下文中的bean。
-//getRootConfigClasses()方法返回带有@Configuration注解的类将会用来配置ContextClassLoaderListener创建的应用的上下文。
-public class ServletContainerInitConfig extends AbstractDispatcherServletInitializer {
+public class ServletContainerInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(SpringmvcConfig.class);
-        return context;
+    protected Class<?>[] getRootConfigClasses() {//获取spring容器配置
+        return new Class[0];
     }
 
     @Override
-    protected String[] getServletMappings() {
+    protected Class<?>[] getServletConfigClasses() {//springmvc容器的配置
+        return new Class[]{SpringmvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {//获取springmvc拦截路径
         return new String[]{"/"};
-    }
-
-    @Override
-    protected WebApplicationContext createRootApplicationContext() {
-        return null;
     }
 }
