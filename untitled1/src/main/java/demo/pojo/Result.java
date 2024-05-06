16537ab82112eb78cfdcffb111698c302567ae07
Administrator <1784264314@qq.com>
2024-05-06 09:27:20 +0800

    66

---
+++ untitled1/src/main/java/demo/pojo/Result.java
@@ -0,0 +1,14 @@
+package demo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+    private String msg;
+    private String code;
+    private Object data;
+}
