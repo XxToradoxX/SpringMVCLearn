6afb35b014300fc2034c18007f0441420cc0afec
XxToradoxX <1784264314@qq.com>
2024-05-07 18:05:01 +0800

    666

--- untitled1/src/main/java/demo/controller/BookController.java
+++ untitled1/src/main/java/demo/controller/BookController.java
@@ -2,17 +2,24 @@ package demo.controller;
 
 import demo.pojo.Book;
 import demo.pojo.Result;
+import demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/book")
 public class BookController {
+    @Autowired
+    DemoService demoService;
+
     @PostMapping
-    public Result GetBook(@RequestBody Book book) {
-        System.out.println(book);
-        return new Result("200200", "find Success", book);
+    public Result GetBook() {
+        List<Book> bookList = demoService.selectAll();
+        return new Result("200200", "获得BOOKLIST", bookList);
     }
 }
