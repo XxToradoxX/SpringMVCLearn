9c823e33677e73b7a3b22efc48597edeb7e728eb
Administrator <1784264314@qq.com>
2024-05-08 11:38:31 +0800

    666

--- untitled1/src/main/java/demo/config/ServletContainerInitConfig.java
+++ untitled1/src/main/java/demo/config/ServletContainerInitConfig.java
@@ -1,5 +1,6 @@
 package demo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
