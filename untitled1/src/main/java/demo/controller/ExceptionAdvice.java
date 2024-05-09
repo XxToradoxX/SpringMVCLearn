a8bdcfefa3c2514e93eb9c03641eef4a9c4974be
Administrator <1784264314@qq.com>
2024-05-09 16:34:34 +0800

    6666

---
+++ untitled1/src/main/java/demo/controller/ExceptionAdvice.java
@@ -0,0 +1,12 @@
+package demo.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(Exception.class)
+    public String handleException(){
+        return "异常,收下了";
+    }
+}
