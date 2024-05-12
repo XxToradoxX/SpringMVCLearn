7184385f351e59cd093f1014f5251b47b29d646f
XxToradoxX <1784264314@qq.com>
2024-05-12 22:46:23 +0800

    666

--- untitled1/src/main/java/demo/controller/ExceptionAdvice.java
+++ untitled1/src/main/java/demo/controller/ExceptionAdvice.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
     public String handleException(){
-        return "异常,收下了";
+        return "Catch EXception";
     }
 }
