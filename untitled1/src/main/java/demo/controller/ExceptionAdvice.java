5b4ee4784b4a04df4968e32ef7e94063a81c438f
Administrator <1784264314@qq.com>
2024-05-13 10:31:16 +0800

    666

--- untitled1/src/main/java/demo/controller/ExceptionAdvice.java
+++ untitled1/src/main/java/demo/controller/ExceptionAdvice.java
@@ -1,13 +1,14 @@
 package demo.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception e){
-        return e.getMessage()+ HttpStatus.BAD_REQUEST;
+    public ResponseEntity handleException(Exception e){
+        return new ResponseEntity<String>(e.getMessage()+ HttpStatus.BAD_REQUEST,HttpStatus.BAD_REQUEST);
     }
 }
