2dc67fe57e3b02286897a5aaabdcc02acd71fb16
Administrator <1784264314@qq.com>
2024-05-16 14:06:42 +0800

    Changes

--- untitled1/src/main/java/demo/config/SpringMvcSupport.java
+++ untitled1/src/main/java/demo/config/SpringMvcSupport.java
@@ -1,6 +1,7 @@
 package demo.config;
 
 import demo.controller.interceptor.MyInterceptor;
+import demo.controller.interceptor.MyInterceptor2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,10 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class SpringMvcSupport extends WebMvcConfigurationSupport {
     @Autowired
     MyInterceptor interceptor;
+    @Autowired
+    MyInterceptor2 interceptor2;
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor).addPathPatterns("/book/*");
-        registry.addInterceptor(interceptor).addPathPatterns("/book");
+        registry.addInterceptor(interceptor2).addPathPatterns("/book/**");
+        registry.addInterceptor(interceptor).addPathPatterns("/book/**");
+//        **代表包括book与book的子
     }
 
 }
